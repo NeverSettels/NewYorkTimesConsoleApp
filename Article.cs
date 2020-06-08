6a1a11094031b6a6a460fb7da56b2360082da523
Eric Settels <settels.eric@gmail.com>
2020-06-08 14:19:26 -0700

    followed lesson but there is an issue of the classic 'bject reference not set.... ect'

---
+++ Article.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace  NewYorkTimesApiConsoleApp
+{
+  class Article 
+  {
+    public string Section {get; set;}
+    public string Title {get; set;}
+    public string Abstract {get;set;}
+    public string Url {get;set;}
+    public string Byline {get;set;}
+  }
+}
